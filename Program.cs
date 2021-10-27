5e7bf7d1400148df49e5c14c6777235360d5cd74
kesquerra <esquerrk@oregonstate.edu>
2021-10-26 18:00:38 -0700

    base code

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Character;
+
+namespace dungeon_crawler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player ch = new Character.Player("Kyle");
+            Console.WriteLine(ch.details());
+        }
+    }
+}
